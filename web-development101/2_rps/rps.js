3fa6e19d55960e6c0cdca1c238adf722cb312022
Shareni <anubis.firepoi@gmail.com>
2021-01-22 17:44:49 +0100

    implemented scoring TODO: clean up playRound

--- web-development101/2_rps/rps.js
+++ web-development101/2_rps/rps.js
@@ -15,6 +15,9 @@ function humanPlays() {
     return choiceLower.charAt(0).toUpperCase() + choiceLower.slice(1);
 }
 
+let scorePlayer = 0;
+let scoreComputer = 0;
+
 function playRound(playerSelection, computerSelection) {
     let youWon = `You won! ${playerSelection} beats ${computerSelection}`;
     let youLost = `You lost! ${computerSelection} beats ${playerSelection}`;
@@ -23,34 +26,40 @@ function playRound(playerSelection, computerSelection) {
         return `It's a tie! You both played ${playerSelection}`;
     } else if (playerSelection === 'Rock') {
         if (computerSelection === 'Scissors') {
+            scorePlayer++;
             return youWon;
         }
         else {
+            scoreComputer++;
             return youLost;
         }
     } else if (playerSelection === 'Paper') {
         if (computerSelection === 'Rock') {
+            scorePlayer++;
             return youWon;
         } else {
+            scoreComputer++;
             return youLost;
         }
     } else if (playerSelection === 'Scissors') {
         if (computerSelection === 'Paper') {
+            scorePlayer++;
             return youWon;
         }
         else {
+            scoreComputer++;
             return youLost;
         }
     }
 }
 
 function game(round) {
-    scorePlayer = 0;
-    scoreComputer = 0;
-    // TO DO: implement score tracking
     for (i = 1; i <= round; i++) {
         console.log(`Round ${i}: ${playRound(humanPlays(), computerPlays())}`);
+        console.log(`The overall the score is ${scorePlayer} for the human, and ${scoreComputer} for the computer`)
     }
+    scoreComputer = 0;
+    scorePlayer = 0;
 }
 
 game(5);
