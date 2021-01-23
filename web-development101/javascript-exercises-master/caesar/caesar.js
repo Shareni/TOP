8d14741d25f6f1acf7ba661986d536cb7aa65554
Shareni <anubis.firepoi@gmail.com>
2021-01-23 23:22:12 +0100

    infitnite loop and no results, but still better

--- web-development101/javascript-exercises-master/caesar/caesar.js
+++ web-development101/javascript-exercises-master/caesar/caesar.js
@@ -1,32 +1,4 @@
 const caesar = function(string, shiftFactor) {
-    function shifter(character, shiftFactor) {
-        let shiftyCharacter = "";
-        if (character >= 65 && character <= 90) {
-            shiftyCharacter = character + (shiftFactor);
-            if (shiftyCharacter > 90) {
-                shiftyCharacter = (64 + ((shiftyCharacter - 90) % 25))
-                return shiftyCharacter;
-            } else if (shiftyCharacter < 65) {
-                shiftyCharacter = 91 - ((90 - shiftyCharacter) % 25);
-                return shiftyCharacter;
-            } else {
-                return shiftyCharacter;
-            }
-        } if (character >= 97 && character <= 122) {
-            shiftyCharacter = character + (shiftFactor);
-            if (shiftyCharacter > 122) {
-                shiftyCharacter = (96 + ((shiftyCharacter - 122) % 25))
-                return shiftyCharacter;
-            } else if (shiftyCharacter < 97) {
-                shiftyCharacter = 123 - ((122 - shiftyCharacter) % 25);
-                return shiftyCharacter;
-            } else {
-                return shiftyCharacter;
-            }
-        } else {
-            return character;
-        }
-    }
     function asciiEncoderDecoder(char) {
         if (typeof (char) === "number") {
             return String.fromCharCode(char);
@@ -34,11 +6,39 @@ const caesar = function(string, shiftFactor) {
             return char.charCodeAt(0);
         }
     }
+    function shifter(character, shiftFactor, upperLimit, lowerLimit) {
+        shiftedLetter = character;
+        if (shiftFactor > 0) {
+            for (i = 0; i < shiftFactor; i++) {
+                shiftedLetter++;
+                if (shiftedLetter > upperLimit) {
+                    shiftedLetter = lowerLimit;
+                }
+            }
+        } else if (shiftFactor < 0) {
+            for (i = 0; i > shiftFactor; i--) {
+                shiftedLetter--;
+                if (shiftedLetter < lowerLimit) {
+                    shiftedLetter = upperLimit;
+                }
+            }
+        }
+    }
+    function sorter(character, shiftFactor) {
+        if (character >= 65 && character <= 90) {
+            return shifter(character, shiftFactor, 65, 90);
+        } else if (character >= 97 && character <= 122) {
+            return shifter(character, shiftFactor, 97, 122);
+        } else {
+            return character;
+        }
+    }
+
     let encryptedString = "";
     for (i = 0; i < string.length; i++) {
         let character = string[i];
         character = asciiEncoderDecoder(character);
-        character = shifter(character, shiftFactor);
+        character = sorter(character, shiftFactor);
         character = asciiEncoderDecoder(character);
         encryptedString = encryptedString + character;
     }
