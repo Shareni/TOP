d8b5f091efcc81b880a271101d7991a3146dc5b1
Shareni <anubis.firepoi@gmail.com>
2021-01-24 03:56:37 +0100

    done

--- web-development101/javascript-exercises-master/fibonacci/fibonacci.js
+++ web-development101/javascript-exercises-master/fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let x = 1;
+    let y = 1
+    if (member < 0) {
+        return "OOPS";
+    } else if (member < 3) {
+        return x;
+    } else {
+        for (i = 1; i < (member / 2); i++) {
+            x = x + y;
+            y = y + x;
+        }
+        if (member % 2 === 0) {
+            return y;
+        } else {
+            return x;
+        }
+    }
 }
 
 module.exports = fibonacci
