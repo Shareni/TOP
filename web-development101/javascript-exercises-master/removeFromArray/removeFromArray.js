3e7abea115d5a74e7b8b06d5d39be60b566ee990
Shareni <anubis.firepoi@gmail.com>
2021-01-28 08:35:06 +0100

    brute force, but works

--- web-development101/javascript-exercises-master/removeFromArray/removeFromArray.js
+++ web-development101/javascript-exercises-master/removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.prototype.slice.call(arguments, 1);
 
+    function checker(item, args) {
+        let isRemoved = false;
+        for (let i = 0; i < args.length; i++) {
+            if (item === args[i]) {
+                isRemoved = true;
+                break;
+            }
+        }
+        return isRemoved;
+    }
+
+    arrayLength = array.length;
+    let returnArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+        let arrayElement = array.pop();
+        let isRemoved = checker(arrayElement, args);
+        if (isRemoved === false) {
+            returnArray.unshift(arrayElement)
+        }
+
+    }
+    return returnArray;
 }
 
 module.exports = removeFromArray
