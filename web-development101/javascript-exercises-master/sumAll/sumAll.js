954b55c550c02f07b5f4b3d214b468dd0fb51d09
Shareni <anubis.firepoi@gmail.com>
2021-01-31 16:43:29 +0100

    done

--- web-development101/javascript-exercises-master/sumAll/sumAll.js
+++ web-development101/javascript-exercises-master/sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let summedUp = 0;
+    let start;
+    let end;
+    if (typeof (x) === 'number' && typeof (y) === 'number' && x >= 0 && y >= 0) {
+        if (x < y) {
+            start = x;
+            end = y;
+        } else {
+            start = y;
+            end = x;
+        }
+        for (start; start < end + 1; start++) {
+            summedUp = summedUp + start;
+        }
+        return summedUp;
+    } else {
+        return 'ERROR';
+    }
 
 }
 
